c5a18059743cd45172bbb4646240ae773b28c1f9
Mahesh4134 <94799710+Mahesh4134@users.noreply.github.com>
2023-12-09 13:31:12 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import time
 
 
 def calculate_distance_matrix(df)->pd.DataFrame():
@@ -12,8 +13,10 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
-
-    return df
+    distance_matrix = distance_matrix + distance_matrix.T
+    distance_matrix.values[[range(distance_matrix.shape[0])]*2] = 0
+    
+    return distance_matrix
 
 
 def unroll_distance_matrix(df)->pd.DataFrame():
@@ -27,8 +30,10 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
+    unrolled_df = df.stack().reset_index()
+    unrolled_df.columns = ['id_start', 'id_end', 'distance']
 
-    return df
+    return unrolled_df
 
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
@@ -44,8 +49,13 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
+    reference_avg_distance = df[df['id_start'] == reference_id]['distance'].mean()
+    threshold = 0.1 * reference_avg_distance
+    result_df = df.groupby('id_start')['distance'].mean().reset_index()
+    result_df = result_df[(result_df['distance'] >= reference_avg_distance - threshold) & 
+                          (result_df['distance'] <= reference_avg_distance + threshold)]
 
-    return df
+    return  result_df 
 
 
 def calculate_toll_rate(df)->pd.DataFrame():
@@ -59,6 +69,11 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
+    df['moto'] = df['distance'] * 0.8
+    df['car'] = df['distance'] * 1.2
+    df['rv'] = df['distance'] * 1.5
+    df['bus'] = df['distance'] * 2.2
+    df['truck'] = df['distance'] * 3.6
 
     return df
 
@@ -74,5 +89,20 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+    weekday_discount_factors = pd.cut(df.index.get_level_values(1).dayofweek, bins=[-1, 0, 10, 18, 24],
+                                      labels=[0.8, 1.2, 0.8, 0.8], right=False).astype(float)
+    weekend_discount_factors = 0.7
+    df['start_day'] = df.index.get_level_values(1).strftime('%A')
+    df['end_day'] = df['start_day']
+    df['start_time'] = time(0, 0)
+    df['end_time'] = time(23, 59, 59)
+    df['moto'] = df['moto'] * weekday_discount_factors
+    df['car'] = df['car'] * weekday_discount_factors
+    df['rv'] = df['rv'] * weekday_discount_factors
+    df['bus'] = df['bus'] * weekday_discount_factors
+    df['truck'] = df['truck'] * weekday_discount_factors
+    weekend_mask = (df['start_day'].isin(['Saturday', 'Sunday']))
+    df.loc[weekend_mask, ['moto', 'car', 'rv', 'bus', 'truck']] *= weekend_discount_factors
+
 
     return df
