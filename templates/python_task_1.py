f16409a750130712a26444da4511c3d107eb6d01
Azeem-code <58939671+Azeem-code@users.noreply.github.com>
2023-12-10 20:55:25 +0530

    Assessment  python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import numpy as np
+
+"Import files from local"
+
+from google.colab import drive 
+drive.mount('/content/googledrive/', force_remount=True)
+
+df=pd.read_csv('/content/dataset-1.csv')
 
 
 def generate_car_matrix(df)->pd.DataFrame:
@@ -12,8 +20,15 @@ def generate_car_matrix(df)->pd.DataFrame:
         pandas.DataFrame: Matrix generated with 'car' values, 
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
-    # Write your logic here
+    #pivot the DataFrame 
+    pivto = df.pivot(index='id_1', columns='id_2', values='car').fillna(0)
 
+     # Fill diagonal values with 0
+    for idx in car_matrix.index:
+        if idx in car_matrix.columns:
+            car_matrix.loc[idx, idx] = 0
+
+    
     return df
 
 
@@ -28,6 +43,12 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+    bins = [-float('inf'), 15, 25, float('inf')]
+    labels = ['low', 'medium', 'high']
+
+    df['car_type'] = car_types
+    print(df)
+
 
     return dict()
 
