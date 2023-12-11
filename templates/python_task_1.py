30d3b96b2d3ed8d524c0c111f54357c60f0cc7d7
Jatinnagar777 <116958453+Jatinnagar777@users.noreply.github.com>
2023-12-11 21:24:58 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,7 +13,8 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
-
+    car_matrix = df.pivot_table(values='car', index='id_1', columns='id_2')
+    car_matrix.fillna(0, inplace=True)
     return df
 
 
@@ -28,8 +29,9 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
-
-    return dict()
+    df['car_type'] = df['car'].apply(lambda x: 'low' if x <= 15 else 'medium' if x <= 25 else 'high')
+    type_counts = df['car_type'].value_counts().sort_index().to_dict()
+    return type_counts
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +45,9 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
-
-    return list()
+    bus_mean = df['bus'].mean()
+    bus_indexes = df.index[(df['bus'] > 2 * bus_mean)].tolist()
+    return sorted(bus_indexes)
 
 
 def filter_routes(df)->list:
@@ -58,8 +61,12 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
-
-    return list()
+    df['avg_truck'] = df['truck'].mean()
+    df['avg_truck'].fillna(0, inplace=True)
+    df_filtered = df[df['avg_truck'] > 7]
+    routes = df_filtered['route'].tolist()
+    routes.sort()
+    return routes
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,7 +80,9 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
-
+    modified_matrix = car_matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+    modified_matrix = modified_matrix.round(1)
+    return modified_matrix
     return matrix
 
 
@@ -88,5 +97,7 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
-
-    return pd.Series()
+    start_date = df[key].min()
+    end_date = df[key].max()
+    return (start_date is not None) and (end_date is not None) and (
+      start_date <= end_date)
